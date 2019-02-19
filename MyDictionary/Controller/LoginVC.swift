fdd5c4c57b4868cb986ed61755b9ecdb20302126
Gökhan Kılıç <gokhankilic@Gokhans-MacBook-Air.local>
2019-02-19 17:33:02 +0300

    UI Designs were completed

---
+++ MyDictionary/Controller/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  MyDictionary
+//
+//  Created by Gökhan Kılıç on 19.02.2019.
+//  Copyright © 2019 Gökhan Kılıç. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
